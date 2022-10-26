a40605f6e58d1d24952b6f4454d743c217f153e3
xyZ <83569693+xyZ1107@users.noreply.github.com>
2022-10-26 17:45:21 -0400

    Add files via upload

---
+++ Team188/host files/index.php
@@ -0,0 +1,6 @@
+<?php include_once("index.html"); ?>
+<?php include_once("about.html"); ?>
+<?php include_once("contact.html"); ?>
+<?php include_once("Outreach.html"); ?>
+<?php include_once("Sponsors.html"); ?>
+<?php include_once("Sponsors-package.html"); ?>
