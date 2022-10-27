d551eaa0fae027a3e3870d59a982ccc4a409b257
xyZ <83569693+xyZ1107@users.noreply.github.com>
2022-10-26 21:19:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,1 @@
 <?php include_once("index.html"); ?>
-<?php include_once("about.html"); ?>
-<?php include_once("contact.html"); ?>
-<?php include_once("Outreach.html"); ?>
-<?php include_once("Sponsors.html"); ?>
-<?php include_once("Sponsors-package.html"); ?>
